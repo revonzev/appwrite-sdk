30f31166333721b8148d0f90f478ced0ce3f91c6
revonzev <revonzev+git@protonmail.com>
2025-09-02 05:29:54 +0700

    fix: JSON parse instead of parse string

--- addons/appwrite-sdk/Realtime/realtime.gd
+++ addons/appwrite-sdk/Realtime/realtime.gd
@@ -46,7 +46,7 @@ func _connected(proto = ""):
 	
 func _on_data():
 	var data: String = _client.get_peer(1).get_packet().get_string_from_utf8()
-	received_updates.emit(parse_json(data))
+	received_updates.emit(JSON.parse_string(data))
 
 func _process(delta):
 	_client.poll()
